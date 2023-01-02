8044f46a96b36508a2f85a8ccc0f6c3f52e568b4
Luis Castillo <>
2023-01-02 04:23:13 -0600

    Last modifications

--- bash/deploy_timeoff.sh
+++ bash/deploy_timeoff.sh
@@ -1,3 +1,3 @@
  #!/bin/bash
 
-aws ec2 describe-instances --filter Name=tag:Name,Values=timeoff  --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text--region us-east-1
+aws ec2 describe-instances --filter Name=tag:Name,Values=timeoff  --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text --region us-east-1
