26ded4095c881493664d0979224ebc7f242256a2
Luis Castillo <>
2023-01-02 03:10:16 -0600

    Last modifications

--- groovy/deploy_timeoff.groovy
+++ groovy/deploy_timeoff.groovy
@@ -15,7 +15,7 @@ pipeline {
                         accessKeyVariable: 'AWS_ACCESS_KEY_ID',
                         secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
                     ]]) {
-                        sh 'sudo aws ec2 describe-security-groups --region us-east-1'  
+                        sh 'aws ec2 describe-security-groups --region us-east-1'  
                     }         
                 }
             }
