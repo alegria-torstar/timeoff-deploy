d998c288d664a94ba304e4c4f363e571ef2c6e69
Luis Castillo <>
2023-01-02 04:28:57 -0600

    Last modifications

--- bash/deploy_timeoff.sh
+++ bash/deploy_timeoff.sh
@@ -4,4 +4,5 @@
 
 for ip in $(aws ec2 describe-instances --filter Name=tag:Name,Values=timeoff  --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text --region us-east-1) ; 
     do echo "Ip is: $ip" ; 
+    # Add code for ssh into all the instances and upgrade the code to its newest version
 done
