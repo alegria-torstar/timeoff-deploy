09fc112b809274265c892022a67c19a94135c8eb
Luis Castillo <>
2023-01-02 03:09:33 -0600

    Last modifications

--- groovy/deploy_timeoff.groovy
+++ groovy/deploy_timeoff.groovy
@@ -9,7 +9,14 @@ pipeline {
          stage("Clone timeoff repo") {
             steps {
                 script {
-                    sh 'sudo aws ec2 describe-security-groups --region us-east-1'           
+                    withCredentials([[
+                        $class: 'AmazonWebServicesCredentialsBinding',
+                        credentialsId: "f3fd2446-5adb-4979-a1d1-2270597b6466",
+                        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                    ]]) {
+                        sh 'sudo aws ec2 describe-security-groups --region us-east-1'  
+                    }         
                 }
             }
         }
