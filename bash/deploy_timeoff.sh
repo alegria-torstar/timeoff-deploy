8d4e1387fa4891461e6530684b9abdff6afc1a59
Luis Castillo <>
2023-01-02 04:19:47 -0600

    Last modifications

--- bash/deploy_timeoff.sh
+++ bash/deploy_timeoff.sh
@@ -1,3 +1,3 @@
  #!/bin/bash
 
-aws ec2 describe-instances --filter Name=tag:Name,Values=ADS-prod-ads --query "Reservations[*].Instances[*].PrivateIpAddress" --output=json --region us-east-1
+aws ec2 describe-instances --filter Name=tag:Name,Values=timeoff  --region us-east-1
