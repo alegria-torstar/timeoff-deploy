55e29965da6288d1728a9f50fc5fe643aab19658
Luis Castillo <>
2023-01-02 15:46:05 -0600

    Last modifications

--- bash/deploy_timeoff.sh
+++ bash/deploy_timeoff.sh
@@ -5,7 +5,7 @@ chmod 600 Operations-Luis.pem
 
 for ip in $(aws ec2 describe-instances --filter Name=tag:Name,Values=timeoff  --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text --region us-east-1) ; 
     do 
-    ssh -i Operations-Luis.pem ec2-user@${ip} << EOF
+    ssh -tt -i Operations-Luis.pem ec2-user@${ip} << EOF
 git clone https://github.com/timeoff-management/timeoff-management-application.git timeoff-management
 cd timeoff-managemenet
 npm install 
