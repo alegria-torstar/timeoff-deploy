fc00e90b69194cdd4dbd8c62a4d055bd17987753
Luis Castillo <>
2023-01-02 04:26:05 -0600

    Last modificationms

--- bash/deploy_timeoff.sh
+++ bash/deploy_timeoff.sh
@@ -1,3 +1,7 @@
  #!/bin/bash
 
-aws ec2 describe-instances --filter Name=tag:Name,Values=timeoff  --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text --region us-east-1
+
+
+for ip in $(aws ec2 describe-instances --filter Name=tag:Name,Values=timeoff  --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text --region us-east-1) ; 
+    do echo "Ip is: $ip" ; 
+done
