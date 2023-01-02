a111ecd7ebaecf37e84deefab249faf1ced393d7
Luis Castillo <>
2023-01-02 15:36:57 -0600

    Last modifications

--- bash/deploy_timeoff.sh
+++ bash/deploy_timeoff.sh
@@ -1,8 +1,14 @@
  #!/bin/bash
 
-
+aws s3 cp . s3://luis-pem-files/Operations-Luis.pem
+chmod 600 Operations-Luis.pem
 
 for ip in $(aws ec2 describe-instances --filter Name=tag:Name,Values=timeoff  --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text --region us-east-1) ; 
-    do echo "Ip is: $ip" ; 
-    # Add code for ssh into all the instances and upgrade the code to its newest version
+    ssh -i Operations-Luis.pem ec2-user@ip << EOF
+git clone https://github.com/timeoff-management/timeoff-management-application.git timeoff-management
+cd timeoff-managemenet
+npm install 
+npm run-script db-update
+npm start
+EOF
 done
