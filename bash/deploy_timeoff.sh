193de714a1f38db0ce1418a86b79c7c354d78542
Luis Castillo <>
2023-01-02 16:05:00 -0600

    Last modifications

--- bash/deploy_timeoff.sh
+++ bash/deploy_timeoff.sh
@@ -6,11 +6,11 @@ chmod 600 Operations-Luis.pem
 for ip in $(aws ec2 describe-instances --filter Name=tag:Name,Values=timeoff  --query "Reservations[*].Instances[*].PrivateIpAddress" --output=text --region us-east-1) ; 
     do 
     echo ${ip}
-    #ssh -tt -o StrictHostKeyChecking=no -i Operations-Luis.pem ec2-user@${ip} << EOF
-#git clone https://github.com/timeoff-management/timeoff-management-application.git timeoff-management
-#cd timeoff-management
-#npm install 
-#npm run-script db-update
-#npm start&
-#EOF
+    ssh -tt -o StrictHostKeyChecking=no -i Operations-Luis.pem ec2-user@${ip} << EOF
+git clone https://github.com/timeoff-management/timeoff-management-application.git timeoff-management
+cd timeoff-management
+npm install 
+npm run-script db-update
+npm start&
+EOF
 done
